2c7151f3ae7fd1f29c323c687e3ac123df07bb36
StraXRobinark <raresugarglider@gmail.com>
2021-10-14 20:37:40 +0800

    Fixed the repo

--- nugget.js
+++ nugget.js
@@ -3,6 +3,7 @@ const Discord = require("discord.js")
 const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]})
 var token = require("./token.json")
 
+/*
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
@@ -12,12 +13,16 @@ client.on("ready", () => {
     status: "idle" }),
   client.user.setActivity("Nugget Castle", {type: "WATCHING"})
 })
+*/
 
 
 client.on("message", msg => {
   if(msg.member.user.tag != client.user.tag)
   {
-    
+      if(msg.content === "nug!help")
+      {
+        msg.channel.send("How may Nugget help you today?")
+      }
   }
 })
 
