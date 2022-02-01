647d892b76613321bdfa48353094faba27cfa5ff
StraXRobinark <raresugarglider@gmail.com>
2022-02-01 13:16:57 +0800

    Changed from Javascript to Python

--- keep_alive.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const app = express()
-app.get("/", (req, res) => {
-  res.send("hello Nugget!")
-})
-
-app.listen(3000, () => {
- console.log("Nugget Connected!")
-})
